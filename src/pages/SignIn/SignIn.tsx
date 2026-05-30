f94c8518195bf1654b995f29c70aedd1ea584766
Montana Gau <montanagau13@gmail.com>
2026-05-30 18:59:58 -0500

    Moved sign in button to sign in page

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,3 +1,16 @@
+import { authenticateGoogle } from "../../api/auth"
+import { GoogleSignInButton } from "../../components/GoogleSignInButton/GoogleSignInButton"
+import styles from './SignIn.module.scss'
+
 export default function SignIn() {
-    return <>Sign In</>
+
+    const onSignIn = (googleCredential: string) => {
+        console.log(googleCredential)
+        const response = authenticateGoogle(googleCredential);
+    }
+
+    return <div className={styles.signIn}>
+        <div className={styles.box}></div>
+        <GoogleSignInButton onSuccess={onSignIn}></GoogleSignInButton>
+    </div>
 }
