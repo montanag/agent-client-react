b8aeca1de9cf7254c3a04072fcca55e0787d2926
Montana Gau <montanagau13@gmail.com>
2026-06-13 13:14:04 -0600

    pr comments

--- src/api/toolsets.api.ts
+++ src/api/toolsets.api.ts
@@ -1,7 +1,6 @@
 import z from "zod";
 import { ToolsetSchema } from "../models/toolsets.model";
 import { request } from "./common";
-import { ToolSchema } from "../models/tools.model";
 
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL
@@ -11,6 +10,6 @@ export async function getToolsets() {
     return await request({ schema: z.array(ToolsetSchema), url: resourceUrl })
 }
 
-export async function getToolByUuid(uuid: string) {
-    return await request({ schema: ToolSchema, url: `${resourceUrl}/${uuid}` })
+export async function getToolsetByUuid(uuid: string) {
+    return await request({ schema: ToolsetSchema, url: `${resourceUrl}/${uuid}` })
 }   
